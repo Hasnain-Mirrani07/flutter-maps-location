2ffa026af89c80c8c2239c8fd9ae7abfb389f5ad
Hasnain-Mirrani07 <122273493+Hasnain-Mirrani-Tecrux@users.noreply.github.com>
2023-02-23 22:21:59 +0500

    .

--- lib/map/map.dart
+++ lib/map/map.dart
@@ -13,7 +13,7 @@ class MapLocationScreen extends StatefulWidget {
 class _MapLocationScreenState extends State<MapLocationScreen> {
   final Completer<GoogleMapController> _controller = Completer();
   static const CameraPosition _kGooglePlex =
-      CameraPosition(target: LatLng(33.7468, -73.376), zoom: 12);
+      CameraPosition(target: LatLng(33.7468, -74.376), zoom: 12);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
